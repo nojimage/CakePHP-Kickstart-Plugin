a421fd586f5001314d37d454ff2f11b7915ae1ab
nojimage <nojimage@gmail.com>
2011-10-15 18:55:58 +0900

    add KickstartCommandTask::plugin

--- vendors/shells/tasks/kickstart_command.php
+++ vendors/shells/tasks/kickstart_command.php
@@ -112,6 +112,7 @@ class KickstartCommandTask extends Shell {
      * get simpletest files
      *
      * @param mixed $params
+     * @deprecated
      */
     public function get_simpletest($params) {
 
@@ -167,6 +168,19 @@ class KickstartCommandTask extends Shell {
     }
 
     /**
+     * setup plugin
+     *
+     * @param mixed $params
+     */
+    public function plugin($params) {
+        if (!isset($params['repo']) || !isset($params['target'])) {
+            $this->err(__d('kickstart', "'plugin' need 'name', 'target', and 'repo' options.", true));
+            return;
+        }
+        $this->git_submodule($params);
+    }
+
+    /**
      * generate file from template
      *
      * @param array $params
@@ -210,6 +224,26 @@ class KickstartCommandTask extends Shell {
         $this->Template->templatePaths = array(dirname(dirname(__FILE__)) . DS . 'templates' . DS);
         // generate file
         $this->createFile(ROOT . DS . $params['target'], $this->Template->generate('generatefiles', $params['template'], $vars));
+    }
+
+    /**
+     * write to bootstrap.php
+     *
+     * @param string $string
+     * @return bool
+     */
+    protected function _writeToBootstrap($string) {
+        $filename = $this->getConfigPath() . 'bootstrap.php';
+        $content = file_get_contents($filename);
+        return file_put_contents($filename, $content . $string);
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getConfigPath() {
+        return CONFIGS;
     }
 
 }
